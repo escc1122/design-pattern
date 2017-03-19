0a8dba4aa4be268febc2948d553433559c1e1484
escc1122 <escc1122@gmail.com>
2017-03-19 18:43:21 +0800

    Create HistogramChart.java

---
+++ Simple Factory Pattern/HistogramChart.java
@@ -0,0 +1,10 @@
+//柱狀圖類：具體產品類  
+class HistogramChart implements Chart {  
+    public HistogramChart() {  
+        System.out.println("創建柱狀圖！");  
+    }  
+
+    public void display() {  
+        System.out.println("顯示柱狀圖！");  
+    }  
+}  
