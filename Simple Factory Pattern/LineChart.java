55bc82e285b42f07830f00a15d060907c2c356db
escc1122 <escc1122@gmail.com>
2017-03-19 18:43:46 +0800

    Create LineChart.java

---
+++ Simple Factory Pattern/LineChart.java
@@ -0,0 +1,10 @@
+//折線圖類：具體產品類  
+class LineChart implements Chart {  
+    public LineChart() {  
+        System.out.println("創建折線圖！");  
+    }  
+
+    public void display() {  
+        System.out.println("顯示折線圖！");  
+    }  
+}  
