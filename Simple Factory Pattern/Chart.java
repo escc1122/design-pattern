f52adb97e4beb2e9319fcc93368a1438446ffba6
escc1122 <escc1122@gmail.com>
2018-12-23 14:39:28 +0800

    修改

--- Simple Factory Pattern/Chart.java
+++
@@ -1,4 +0,0 @@
-//抽象圖表接口：抽象產品類  
-interface Chart {  
-    public void display();  
-}  
--- Simple Factory Pattern/Client.java
+++
@@ -1,8 +0,0 @@
-class Client {  
-    public static void main(String args[]) {  
-        Chart chart;  
-        String type = XMLUtil.getChartType(); //讀取配置文件中的參數  
-        chart = ChartFactory.getChart(type); //創建產品對象  
-        chart.display();  
-    }  
-}
