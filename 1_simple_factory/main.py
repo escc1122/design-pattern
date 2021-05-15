770bbd5d2c226602b03562c1c46a608271aaac3c
escc1122 <escc1122@gamil.com>
2021-05-15 20:42:50 +0800

    simple factory

---
+++ 1_simple_factory/main.py
@@ -0,0 +1,65 @@
+import abc
+
+
+class ISimpleFactory(abc.ABC):
+    def __init__(self):
+        self.__numberA: float
+        self.__numberB: float
+
+    @abc.abstractmethod
+    def to_do(self):
+        pass
+
+    @property
+    def numberA(self):
+        return self.__numberA
+
+    @numberA.setter
+    def numberA(self, new_data):
+        self.__numberA = new_data
+
+    @property
+    def numberB(self):
+        return self.__numberB
+
+    @numberB.setter
+    def numberB(self, new_data):
+        self.__numberB = new_data
+
+
+class Add(ISimpleFactory):
+    def __init__(self):
+        super().__init__()
+
+    def to_do(self) -> float:
+        return self.numberA + self.numberB
+
+
+class Sub(ISimpleFactory):
+    def __init__(self):
+        super().__init__()
+
+    def to_do(self) -> float:
+        return self.numberA - self.numberB
+
+
+class SimpleFactory:
+    @staticmethod
+    def create(factory_type):
+        if factory_type == 'add':
+            return Add()
+        elif factory_type == 'sub':
+            return Sub()
+
+
+if __name__ == '__main__':
+    simpleFactory = SimpleFactory()
+    add = simpleFactory.create("add")
+    add.numberA = 10
+    add.numberB = 5
+    print(add.to_do())  # 15
+
+    sub = simpleFactory.create("sub")
+    sub.numberA = 10
+    sub.numberB = 5
+    print(sub.to_do())  # 5
