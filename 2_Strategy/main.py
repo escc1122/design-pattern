3fba3d4b79e77d2ae0e46e8e2ae1e0f3aa5016c4
escc1122 <escc1122@gamil.com>
2021-05-15 21:01:01 +0800

    Strategy

---
+++ 2_Strategy/main.py
@@ -0,0 +1,44 @@
+import abc
+
+
+class IStrategy(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def to_do(self, number_a, number_b) -> float:
+        pass
+
+
+class Add(IStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def to_do(self, number_a, number_b) -> float:
+        return number_a + number_b
+
+
+class Sub(IStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def to_do(self, number_a, number_b) -> float:
+        return number_a - number_b
+
+
+class Strategy:
+    def __init__(self, strategy: IStrategy):
+        self.__strategy = strategy
+
+    def to_do(self, number_a, number_b) -> float:
+        return self.__strategy.to_do(number_a, number_b)
+
+
+if __name__ == '__main__':
+    add = Strategy(Add())
+
+    print(add.to_do(10, 5))  # 15
+
+    sub = Strategy(Sub())
+
+    print(sub.to_do(10, 5))  # 5
