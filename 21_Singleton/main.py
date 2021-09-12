1625a962abc5f5dc8c8f01e2b02022dbd7a2a6d0
escc1122 <escc1122@gamil.com>
2021-09-12 20:07:23 +0800

    update Singleton

--- 21_Singleton/main.py
+++ 21_Singleton/main.py
@@ -1,13 +1,20 @@
+import threading
+import time
+import threading
+
+
 class Singleton:
-    _instance = None
+    _instance_lock = threading.Lock()
 
     def __init__(self):
-        pass
+        time.sleep(2)
 
     @staticmethod
     def get_instance():
-        if Singleton._instance is None:
-            Singleton._instance = Singleton()
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = Singleton()
         return Singleton._instance
 
 
@@ -25,19 +32,24 @@ class Test:
         pass
 
 
+def task(arg):
+    obj = Singleton.get_instance()
+    print(obj)
+
+
 if __name__ == '__main__':
-    a = Singleton.get_instance()
-    b = Singleton.get_instance()
-    print(a)
-    print(b)
-    a = Singleton2()
-    b = Singleton2()
-    print(a)
-    print(b)
-    a = Test()
-    b = Test()
-    print(a)
-    print(b)
+    for i in range(10):
+        t = threading.Thread(target=task, args=[i, ])
+        t.start()
+
+    # a = Singleton2()
+    # b = Singleton2()
+    # print(a)
+    # print(b)
+    # a = Test()
+    # b = Test()
+    # print(a)
+    # print(b)
 # <__main__.Singleton object at 0x00000245C7D70FA0>
 # <__main__.Singleton object at 0x00000245C7D70FA0>
 # <__main__.Singleton2 object at 0x00000245C7DC5640>
