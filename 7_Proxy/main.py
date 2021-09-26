c01dd1ca1d3941804694168f605d65ea32b75d0a
escc1122 <escc1122@gamil.com>
2021-09-26 19:07:20 +0800

    update proxy 修正錯誤

--- 7_Proxy/main.py
+++ 7_Proxy/main.py
@@ -27,18 +27,15 @@ class Sub(IProxy):
 
 
 class Proxy(IProxy):
-    def __init__(self, proxy: IProxy):
-        self.__proxy = proxy
+    def __init__(self):
+        self.__proxy = Add()
 
     def to_do(self, number_a, number_b) -> float:
         return self.__proxy.to_do(number_a, number_b)
 
 
 if __name__ == '__main__':
-    add = Proxy(Add())
+    add = Proxy()
 
     print(add.to_do(10, 5))  # 15
 
-    sub = Proxy(Sub())
-
-    print(sub.to_do(10, 5))  # 5
