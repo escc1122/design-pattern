8bb9be6c7c17fd0c11911b283f30e604ab1e8a45
escc1122 <escc1122@gamil.com>
2021-09-27 23:13:30 +0800

    update Facade

--- 12_Facade/main.py
+++ 12_Facade/main.py
@@ -1,43 +1,31 @@
-import abc
+class Facade:
+    def __init__(self):
+        self.__subClassOne = SubClassOne()
+        self.__subClassTwo = SubClassTwo()
+
+    def flow_a_to_d(self):
+        self.__subClassOne.to_do_a()
+        self.__subClassOne.to_do_b()
+        self.__subClassTwo.to_do_c()
+        self.__subClassTwo.to_do_d()
 
 
-class Facade(abc.ABC):
-    class __SubClassOne(abc.ABC):
-        @classmethod
-        def to_do_a(cls):
-            print("SubClassOne to_do_a")
+class SubClassOne:
+    def to_do_a(self):
+        print("SubClassOne to_do_a")
 
-        @classmethod
-        def to_do_b(cls):
-            print("SubClassOne to_do_b")
+    def to_do_b(self):
+        print("SubClassOne to_do_b")
 
-    class __SubClassTwo(abc.ABC):
-        @classmethod
-        def to_do_c(cls):
-            print("SubClassTwo to_do_c")
 
-        @classmethod
-        def to_do_d(cls):
-            print("SubClassTwo to_do_d")
+class SubClassTwo:
+    def to_do_c(self):
+        print("SubClassTwo to_do_c")
 
-    @classmethod
-    def facade_a(cls):
-        cls.__SubClassOne().to_do_a()
-        cls.__SubClassTwo().to_do_c()
-
-    @classmethod
-    def facade_b(cls):
-        cls.__SubClassOne().to_do_b()
-        cls.__SubClassTwo().to_do_d()
+    def to_do_d(self):
+        print("SubClassTwo to_do_d")
 
 
 if __name__ == '__main__':
     facade = Facade()
-    # SubClassOne to_do_a
-    # SubClassTwo to_do_c
-    # == == == == == == == == == == == ==
-    # SubClassOne to_do_b
-    # SubClassTwo to_do_d
-    facade.facade_a()
-    print("========================")
-    facade.facade_b()
+    facade.flow_a_to_d()
